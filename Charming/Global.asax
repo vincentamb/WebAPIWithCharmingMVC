d0bde4755f26bcbdad98d3298bd5bc09595b3b2f
vincentamb <33020904+vincentamb@users.noreply.github.com>
2017-12-28 18:36:59 +0530

    Initial commit

---
+++ Charming/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Charming.MvcApplication" Language="C#" %>
---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<StudentModel> Get()
+        {
+            List<StudentModel> list = new List<StudentModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                StudentModel stu = new StudentModel();
+                stu.StudentId = i;
+                stu.StuName = "stu" + i;
+                list.Add(stu);
+            }
+            return list;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
